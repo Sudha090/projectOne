fda8d5ef44e8121e6382a4e67d277fac72cf60f6
sudha090 <sudhathatavarthi09@gmail.com>
2026-04-08 00:37:17 +0530

    update the index and home js files to add the global scope and global object

---
+++ home.js
@@ -0,0 +1,11 @@
+
+// global object window and this keyword in JavaScript 
+// var a=10;
+// function b(){
+//     var x = 2;
+//     console.log("Hello World");
+// }
+// console.log(window.a);
+// console.log(this.a);
+// console.log(window.b);
+// console.log(x);
--- index.js
+++ index.js
@@ -10,15 +10,31 @@
     // hoisting --     it is a mechanism where variables and functions are moved to the top of their scope before code execution.   
     // variabes and functions are hoisted to the top of their scope, but only the declarations are hoisted, not the initializations.  
     
-    console.log(getName);
-    getName();
-    console.log(x);
-    var x=5;
-    function getName(){
-        console.log("Hello World");
-    }
     // console.log(getName);
+    // getName();
+    // console.log(x);
+    
+    // var x=5;
+    // function getName(){
+    //     console.log("Hello World");
+    // }
+    // // console.log(getName);
    
     // console.log(getName);is a function declaration, so it is hoisted to the top of the scope.so it will print the function definition to the console.    
     // getName(); is a function call, so it will execute the function and print "Hello World" to the console.
     // console.log(x); is a variable declaration, so it is hoisted to the top of the scope, but it is not initialized until the line var x=5; is executed. Therefore, it will print undefined to the console before it is assigned the value 5.            
+
+
+    //function invocation and variable environment
+    var x=1;
+    a();
+    b();
+    console.log(x);
+    function a(){
+        var x=10;
+        console.log(x);
+    }
+    function b(){
+        var x=100;
+        console.log(x);
+    }   
